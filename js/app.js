f74c5b56b06174b6fbca8e6011373ee56b635459
Jay Silvas <jaywsilvas@gmail.com>
2018-02-15 17:04:14 -0800

    Additional sales table features

--- js/app.js
+++ js/app.js
@@ -3,9 +3,7 @@
 var times = ['6am','7am','8am','9am','10am','11am','12am','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
 var storeList = [];
 var cookiesTable = document.getElementById('cookies');
-
-
-
+var hourlyTotals = [];
 
 // object constructor for each store
 function Store(storeName, minCustPerHour, maxCustPerHour, avgCookPerCust) {
@@ -19,7 +17,6 @@ function Store(storeName, minCustPerHour, maxCustPerHour, avgCookPerCust) {
   storeList.push(this);
   this.calcCustPerHour();
   this.calcCookies();
-  //this.render();    Why do I not need to invoke this method inside the constructor??? Where is it invoked??
 }
 
 // Methods within the constructor
@@ -37,9 +34,9 @@ Store.prototype.calcCookies = function() { // Calculates cookies sold each day
     this.dayTotalCookies += hourlyCookies;
   }
 };
+
 // Table Element Render Functions
 Store.prototype.render = function() {
-  this.calcCookies();
   var trEl = document.createElement('tr');
   var tdEl = document.createElement('td');
 
@@ -82,33 +79,64 @@ function headerRow() {
   trEl.appendChild(thEl);
 
   // Append to Table
-  cookiesTable.append(trEl);
+  cookiesTable.appendChild(trEl);
 }
 
-function cookieRows() {
-
+function renderData() {
   for (var i = 0; i < storeList.length; i++) {
     storeList[i].render();
   }
 }
-/*
-function footerRow() {
-  var trEl = document.createElement('tr');
-  var tfootEl = document.createElement('tfoot');
 
-  // Create Store Column Total Footer
-  tfootEl.textContent = 'Hourly Totals';
-  trEl.appendChild(tfootEl);
-
+function calcHourlyTotals() {
+  hourlyTotals = [];
   for(var i = 0; i < times.length; i++) {
     var total = 0;
     for(var j = 0; j < storeList.length; j++) {
       total += storeList[j].cookiesPerHour[i];
       console.log(total, 'inner loop');
     }
+    hourlyTotals.push(total);
+    console.log('outer for loop total: ' + total);
   }
+  console.log('hourlyTotals: ' + hourlyTotals);
 }
-*/
+
+function calcTotalSum() {
+  var totalSum = 0;
+  for (var i = 0; i < times.length; i++) {
+    totalSum += hourlyTotals[i];
+  }
+  console.log('totalSum: ' + totalSum);
+  return totalSum;
+}
+
+function footerRow() {
+  calcHourlyTotals();
+  var trEl = document.createElement('tr');
+  var tdEl = document.createElement('td');
+
+  tdEl.textContent = 'Hourly Totals';
+  trEl.appendChild(tdEl);
+
+  for (var i = 0; i < times.length; i++) {
+    tdEl = document.createElement('td');
+    tdEl.textContent = hourlyTotals[i];
+    trEl.appendChild(tdEl);
+  }
+  tdEl = document.createElement('td');
+  tdEl.textContent = calcTotalSum();
+  trEl.appendChild(tdEl);
+
+  cookiesTable.appendChild(trEl);
+}
+
+function resetTable() {
+  cookiesTable.innerHTML = '';
+  headerRow();
+  renderData();
+  footerRow();
+}
 
 // Instantiate constructor Store() for each set of inputs
 new Store('First & Pike', 23, 65, 6.3);
@@ -118,7 +146,38 @@ new Store('Capitol Hill', 20, 38, 2.3);
 new Store('Alki Beach', 3, 24, 1.2);
 
 headerRow();
-cookieRows();
-// footerRow();
+renderData();
+footerRow();
 
 console.table(storeList);
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// Global variables for DOM access and such
+var salesForm = document.getElementById('salesForm');
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++
+// This function is the event handler for submitting sales data
+function handleDataSubmit(event) {
+  console.log('log of the event object: ', event);
+  console.log('log of the event.target: ', event.target);
+
+  event.preventDefault();// gotta have it for this purpose. prevents page reload on a 'submit' event
+
+  if (!event.target.where.value || !event.target.min.value || !event.target.max.value || !event.target.avg.value) {
+    return alert('Fields cannot be empty!');
+  }
+  var submitWhere = event.target.where.value;
+  var submitMin = parseInt(event.target.min.value);
+  var submitMax = parseInt(event.target.max.value);
+  var submitAvg = parseFloat(event.target.avg.value);
+  // This empties the form fields after the data has been grabbed
+  event.target.reset();
+  // instantiate user input array in Store constructor
+  new Store(submitWhere, submitMin, submitMax, submitAvg);
+  //Recalculate and re-render table
+  resetTable();
+  console.table(storeList);
+}
+
+// Event listener for form "submit yer data" button
+salesForm.addEventListener('submit', handleDataSubmit);
